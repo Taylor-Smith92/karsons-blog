0fcba3f7ebafc8b12848c65690b5fa67418109e2
Taylor Smith <taylorleesmith92@gmail.com>
2019-01-23 10:54:16 -0700

    added basic structure for html and content

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Milestone 1</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, and Goal</h1>
+		<p><strong>Purpopse:</strong> </p>
+		<p><strong>Audience:</strong> </p>
+		<p><strong>Goal:</strong> </p>
+		<h1>Persona</h1>
+		<p><strong>Name:</strong> <br> <strong>Age:</strong> </p>
+		<p><strong>:</strong> </p>
+		<p><strong>:</strong> </p>
+		<p><strong>:</strong> </p>
+		<p><strong>:</strong> </p>
+	</body>
+</html>
