05a9cef9371469c320cc3d9c3c5be03813a7499b
Taylor Smith <taylorleesmith92@gmail.com>
2019-01-23 14:32:28 -0700

    rough draft of all parameters

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,14 +6,21 @@
 	</head>
 	<body>
 		<h1>Purpose, Audience, and Goal</h1>
-		<p><strong>Purpopse:</strong> </p>
-		<p><strong>Audience:</strong> </p>
-		<p><strong>Goal:</strong> </p>
+		<p><strong>Purpopse:</strong> This project will be part of my professional portfolio as a demonstration of my front end design capabilities.</p>
+		<p><strong>Audience:</strong> This site is meant to be seen by tech industry professionals, other industry professionals, and by anyone else interested
+			in the blog's content matter. </p>
+		<p><strong>Goal:</strong> This website will function as a hub for taste makers and influencers, with content provided by a third party,
+			and as high quality reference for myself to obtain new clients and/or new career opportunities.</p>
 		<h1>Persona</h1>
-		<p><strong>Name:</strong> <br> <strong>Age:</strong> </p>
-		<p><strong>:</strong> </p>
-		<p><strong>:</strong> </p>
-		<p><strong>:</strong> </p>
-		<p><strong>:</strong> </p>
+		<p><strong>Name:</strong> Jessica Ferrari<br> <strong>Age:</strong> 22</p>
+		<p><strong>Occupation:</strong> Entry Level/ Internship</p>
+		<p><strong>Technology:</strong> iPhoneX, MacBookPro 10.13.6</p>
+		<p><strong>Attitudes and Behaviors:</strong> Likes expensive things, dad cut her of/ college/ first time on her own, wants to have style but can't afford
+			to buy whatever she wants to anymore, starting to like DIY crafts.</p>
+		<p><strong>Frustrations and Needs:</strong> Jessica's favorite blogger just retired and she is starving for new and great content. She follows many other
+			blogs but they are no longer curating the perfect set of content that she is expecting. Tumblr is over run with millennials. She wants quality output
+			from someone who has their life together and thinks that will only happen on a private site.</p>
+		<p><strong>Goals:</strong> Jessica wants to view a blog with a professional and elegant theme that contains new and exciting content.</p>
+		<p><strong>User Story:</strong> Jessica Ferrari selects her new favorite blog based on the quality of wep page design and content.</p>
 	</body>
 </html>
