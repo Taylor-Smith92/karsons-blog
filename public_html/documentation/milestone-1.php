3b22006a2944ab4e723ce3b290abdca32dfc5ea7
Taylor Smith <taylorleesmith92@gmail.com>
2019-01-23 21:57:55 -0700

    revised initial draft, fixed incomplete sentences, finalized persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,19 +8,28 @@
 		<h1>Purpose, Audience, and Goal</h1>
 		<p><strong>Purpopse:</strong> This project will be part of my professional portfolio as a demonstration of my front end design capabilities.</p>
 		<p><strong>Audience:</strong> This site is meant to be seen by tech industry professionals, other industry professionals, and by anyone else interested
-			in the blog's content matter. </p>
+			in the blog's content matter.</p>
 		<p><strong>Goal:</strong> This website will function as a hub for taste makers and influencers, with content provided by a third party,
 			and as high quality reference for myself to obtain new clients and/or new career opportunities.</p>
 		<h1>Persona</h1>
 		<p><strong>Name:</strong> Jessica Ferrari<br> <strong>Age:</strong> 22</p>
-		<p><strong>Occupation:</strong> Entry Level/ Internship</p>
+		<p><strong>Occupation:</strong> Internship at a fashion design company</p>
 		<p><strong>Technology:</strong> iPhoneX, MacBookPro 10.13.6</p>
-		<p><strong>Attitudes and Behaviors:</strong> Likes expensive things, dad cut her of/ college/ first time on her own, wants to have style but can't afford
-			to buy whatever she wants to anymore, starting to like DIY crafts.</p>
-		<p><strong>Frustrations and Needs:</strong> Jessica's favorite blogger just retired and she is starving for new and great content. She follows many other
-			blogs but they are no longer curating the perfect set of content that she is expecting. Tumblr is over run with millennials. She wants quality output
-			from someone who has their life together and thinks that will only happen on a private site.</p>
-		<p><strong>Goals:</strong> Jessica wants to view a blog with a professional and elegant theme that contains new and exciting content.</p>
+		<p><strong>Attitudes and Behaviors:</strong> Jessica likes to explore the world and find new and interesting things to learn about. She loves to cook for her
+			friends and family and is always looking for new recipes to try out. She just graduated from college and got her first real job in a new city at an up
+			and coming fashion brand. Jessica is on her own fo the first time in her life but she loves it. Although she is nervous about the future she is also
+			excited to finally get to decorate however she wants to.
+		</p>
+		<p><strong>Frustrations and Needs:</strong> Although Jessica's new job is something that she wants to be doing, the pay is not good and she can't afford
+			nealy any of the design elements she has envisioned for her home or life. As a designer Jessica needs to find DIY tutorials that result in something
+			up to her heightened expectations. Also to her dismay Jessica's favorite blogger has recently started posting less and less often, although the content
+			is still good it is not DIY at all and is of no real use to Jessica. This seems to be the trend with all the other blogs that Jessica has found. She
+			is beginning to think that no one can provide to her the content or community the she knows she deserves. Jessica has found that sites like Tumblr
+			have too many users to search through and that most are either amateurs or frauds. She needs quality output from someone who has their life together
+			and thinks that will only happen on a private and secure website.
+		</p>
+		<p><strong>Goals:</strong> Jessica wants to view a blog with a professional and elegant theme that contains new and exciting content that is on brand with
+			her sense of fashion and design.</p>
 		<p><strong>User Story:</strong> Jessica Ferrari selects her new favorite blog based on the quality of wep page design and content.</p>
 	</body>
 </html>
