12aca918e7a839389b4be10dbd1a5ba5187901c7
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-04 12:56:31 -0700

    revised initial draft, fixed incomplete sentences, finalized persona

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<h1>Milestone 2</h1>
+		<h2>Content Strategy</h2>
+		<p>My site will be one page and have the following sections:</p>
+		<p><strong>Welcome:</strong> When the site opens at the top of the page will be a picture and the page name printed on top. As well as a brief description of what kind of a blog it is.</p>
+		<p><strong>Menu:</strong> There will be a menu that when selected overlays the page content. It will include links to other social media
+		accounts held by the blogger. It will also include a page jump link to the "contact me" form at the bottom of the page. The menu bar will be positioned under the site's main
+		graphic but when the page is scrolled the menu bar will pin to the top of the display, making it accessible anywhere.</p>
+		<p><strong>Content:</strong> This blog's posts will be organized in a single column ordered from most recent at the top. Most blog posts will not have any functionality aside from
+		photo posts will display full resolution versions of the photos when clicked.</p>
+		<p><strong>Contact Me:</strong> At the end of the web page will be a form named "Contact Me". It will not contain personal information about the blogger. It will mainly
+		be used for end users to leave private messages for the blogger.</p>
+		<h2>Wire Frames</h2>
+	</body>
+</html>
