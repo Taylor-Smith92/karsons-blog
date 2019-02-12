b266997a1fa2cedfacc405cbb681c5d3fea89b27
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-12 15:42:56 -0700

    added some boilerplate

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<title>All in Good Taste</title>
+	</head>
+	<body>
+
+	</body>
+</html>
