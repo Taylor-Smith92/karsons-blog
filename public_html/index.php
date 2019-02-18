c9e2d04cf3c0e23be7851161e022360b41450046
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-18 13:08:28 -0700

    added bootstrap links; added some basic html structure to the page

--- public_html/index.php
+++ public_html/index.php
@@ -3,9 +3,43 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous" />
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
 		<title>All in Good Taste</title>
 	</head>
 	<body>
+		<header class="container-fluid bg-warning p-5">
+			<img src="#" alt="main page graphic">
+		</header>
+		<section class="container-fluid navbar bg-secondary">
+			<p>Navigator</p>
+		</section>
+		<section id="posts" class="container bg-secondary p-5">
+			<div class="card">
+				<div class="card-content">
+					<p>text</p>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-content">
+					<p>text</p>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-content">
+					<p>text</p>
+				</div>
+			</div>
+		</section>
+		<section id="contactForm" class="container bg-secondary text-warning" name="contact form">
+			<p class="m-0">This will be the contact form</p>
+		</section>
+		<footer id="footer" class="container-fluid bg-warning p-5">
 
+		</footer>
 	</body>
 </html>
