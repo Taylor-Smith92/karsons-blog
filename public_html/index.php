ee463f7d4bcdc195adeb5886314c8ab9c7bd63d6
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-21 16:43:55 -0700

    tried to make fancybox work

--- public_html/index.php
+++ public_html/index.php
@@ -5,9 +5,14 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous" />
 
+		<link rel="stylesheet" type="text/css" href="jquery.fancybox.min.css">
+
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="jquery.fancybox.min.js"></script>
 
 		<title>All in Good Taste</title>
 	</head>
@@ -43,8 +48,10 @@
 			</div>
 			<div class="card card-content border border-primary m-3">
 				<div class="row">
-					<div class="col">
-						<img class="card-img-top" src="arizona-clouds-dawn-248174.jpg" alt="beautiful picture"/>
+					<div class="col imglist">
+						<a id="button" href="arizona-clouds-dawn-248174.jpg" data-fancybox data-caption="beautiful picture">
+							<img class="card-img-top" src="arizona-clouds-dawn-248174.jpg" alt="beautiful picture"/>
+						</a>
 					</div>
 				</div>
 				<div class="row">
