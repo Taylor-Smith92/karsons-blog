153f37b29ade5fd3d5e4314d021cca086d6c8d03
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-26 12:59:29 -0700

    added files for contact form and recapcha

--- public_html/index.php
+++ public_html/index.php
@@ -4,26 +4,45 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<link rel="stylesheet" href="styles.css" type="text/css">
+
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous" />
-
-
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
 		<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
 
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+		<!-- Fancy Box Links-->
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.6/dist/jquery.fancybox.min.css" />
 		<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.6/dist/jquery.fancybox.min.js"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 
 
 		<title>All in Good Taste</title>
 	</head>
 	<body>
 		<!--Header-------------------------------------------------->
-		<header class="container-fluid p-5">
-			<img src="#" alt="main page graphic">
+		<header class="container-fluid">
+			<div class="row">
+				<div class="col"></div>
+					<div class="col logo">
+						<img class="img-fluid fake-logo" src="fake-logo.png" alt="main page graphic">
+					</div>
+				<div class="col"></div>
+			</div>
 		</header>
+
+		<!--Navigation-Bar--------------------------------------------->
 		<nav class="navbar navbar-expand-lg sticky-top">
 			<a class="navbar-brand" href="#">Home or Icon</a>
 			<ul class="nav nav-pills">
@@ -57,7 +76,7 @@
 			</div>
 			<div class="card card-content m-3">
 				<div class="row">
-					<div class="col imglist">
+					<div class="col">
 						<a id="button" href="arizona-clouds-dawn-248174.jpg" data-fancybox data-caption="beautiful picture">
 							<img class="img-fluid" src="arizona-clouds-dawn-248174.jpg" alt="beautiful picture"/>
 						</a>
@@ -97,10 +116,63 @@
 				</div>
 			</div>
 		</section>
+
 		<!--Contact-Form-------------------------------------------------->
 		<section class="container bg-secondary text-warning" id="contact-form" name="contact form">
-			<p class="m-0">This will be the contact form</p>
+			<form id="contact-form" action="php/mailer.php" method="post">
+				<div class="form-group">
+					<lable for="contactName">Name</lable>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"></span>
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="contactName" name="contactName" placeholder="Name">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<lable for="contactEmail">Email</lable>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"></span>
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<input type="email" class="form-control" id="contactEmail" name="contactEmail" placeholder="Email">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<lable for="contactSubject">Subject</lable>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"></span>
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="contactSubject" name="contactSubject" placeholder="Subject">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<lable for="contactMessage">Message</lable>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"></span>
+							<i class="fa fa-comment" aria-hidden="true"></i>
+						</div>
+						<textarea type="form-control" rows="5" class="form-control" id="contactMessage" name="contactMessage" placeholder="Message"></textarea>
+					</div>
+				</div>
+
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="6LfOEpQUAAAAAPOFlREZfqsC4CNH7DMxhTxzRRYx"></div>
+				<!-- Buttons -->
+				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+			</form>
+			<div id="output-area"></div>
 		</section>
+
 		<!--Footer-------------------------------------------------->
 		<footer class="container-fluid p-5">
 			<div class="row">
